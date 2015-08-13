e9ac21408a7be1533a3bdb22350a7595f91d1f40
kyle roux <kyle@level2designs.com>
2015-08-13 12:37:59 -0700

    added stuff

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ config = {
     'name':'it-ebook-dl',
     'version':'0.0.3',
     'py_modules':['get_pdfs'],
+    'install_requires':[
+         'mechanize==0.2.5',
+         'twill==0.9',
+    ],
     'entry_points':{
         'console_scripts':
             ['find-pdfs = get_pdfs:main']
